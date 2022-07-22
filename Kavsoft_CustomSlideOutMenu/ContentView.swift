15c342f8a622d612e7c00c4c28ee51fdfa574f1f
Inwoo Hwang <dlsdn1207@gmail.com>
2022-07-22 13:19:34 +0900

    Initial Commit

---
+++ Kavsoft_CustomSlideOutMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kavsoft_CustomSlideOutMenu
+//
+//  Created by 황인우 on 2022/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kavsoft_CustomSlideOutMenu/Kavsoft_CustomSlideOutMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kavsoft_CustomSlideOutMenuApp.swift
+//  Kavsoft_CustomSlideOutMenu
+//
+//  Created by 황인우 on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Kavsoft_CustomSlideOutMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
